751da985c7fb061f6f8f624643fd5d12a16455c5
Eric Dawson <edawson42@gmail.com>
2018-09-16 22:24:28 -0700

    added navbar animations and 2 color change functions

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-var button = document.querySelector('.button');
+var button1 = document.querySelector('#button1');
 var background = document.querySelector('.container');
+var button2 = document.querySelector('#button2');
+var navColor = document.querySelector('#navbar');
 
-button.onclick = function () {
+button1.onclick = function () {
  var red = Math.floor(Math.random() * 256);
  var blue = Math.floor(Math.random() * 256);
  var green = Math.floor(Math.random() * 256);
 
  background.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 };
+
+button2.onclick = function () {
+    var red = Math.floor(Math.random() * 256);
+    var blue = Math.floor(Math.random() * 256);
+    var green = Math.floor(Math.random() * 256);
+   
+    navColor.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
+   };
