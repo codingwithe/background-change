c45d00beed63f2ef904e9ea6de4894e5fba058d6
Eric Dawson <edawson42@gmail.com>
2018-09-16 22:37:38 -0700

    made black and white added shadow to text

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ var button2 = document.querySelector('#button2');
 var navColor = document.querySelector('#navbar');
 
 button1.onclick = function () {
- var red = Math.floor(Math.random() * 256);
- var blue = Math.floor(Math.random() * 256);
- var green = Math.floor(Math.random() * 256);
+ var shade = Math.floor(Math.random() * 256);
+ var red = shade;
+ var blue = shade;
+ var green = shade;
 
  background.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
 };
 
 button2.onclick = function () {
-    var red = Math.floor(Math.random() * 256);
-    var blue = Math.floor(Math.random() * 256);
-    var green = Math.floor(Math.random() * 256);
+    var shade = Math.floor(Math.random() * 256);
+    var red = shade;
+    var blue = shade;
+    var green = shade;
    
     navColor.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
    };
